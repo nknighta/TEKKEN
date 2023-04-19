68b439df07b7c44e5bc6d08520c44dfee2aa62a3
NknightA <n.knight.pc0627@gmail.com>
2023-04-19 11:43:02 +0900

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+    let test = "<";
+    let test2 = ">";
   return (
     <>
       <Head>
@@ -14,101 +18,64 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+      <main>
+          <div className={styles.main}>
+              <h1 className={styles.title}>
+                  弁当注文アプリモック 1
+              </h1>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+          <div className={styles.maincontent}>
+              <div className={styles.sub}>
+                    <div className={styles.maincontent_date}>
+                        <h1> {test}   2022/04   {test2} 2</h1>
+                    </div>
+                  <div className={styles.maincontent_inner}>
+                    <Calendmox/>
+                      <Calendmox/>
+                  </div>
+              </div>
+          </div>
       </main>
     </>
   )
+}
+
+const Calendmox = () => {
+    return (
+        <div style={{margin: 30}}>
+            <h1>Aランチ 3</h1>
+            <div style={{ backgroundColor: "black" ,
+                        color: "white",
+                        borderRadius: 10 ,
+                        padding: 14,
+                        height: 300,
+                        width: 500}}>
+                image 4
+            </div>
+            <div style={{
+                backgroundColor: "black" ,
+                color: "white",
+                height: 100,
+                marginTop: 20,
+                borderRadius: 10 ,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+            }}>
+                <h1>カートに入れる 5</h1>
+            </div>
+            <div style={{
+                backgroundColor: "black" ,
+                color: "white",
+                height: 100,
+                marginTop: 20,
+                borderRadius: 10 ,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+            }}>
+                <h1>まとめて注文する 6</h1>
+            </div>
+        </div>
+    );
 }
